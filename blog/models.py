6fa99e9e9c329cd281db41ea2f6dfba9eeccc0ef
fahadkhan98 <fahadkhancsebd@gmail.com>
2024-01-19 00:40:49 +0600

    [Test] Test

--- blog/models.py
+++ blog/models.py
@@ -53,6 +53,9 @@ class Post(models.Model):
    # def get_absolute_url(self):
    #    return reverse('blog:post_detail',
    #    args=[self.id])
+ # def get_absolute_url(self):
+   #    return reverse('blog:post_detail',
+   #    args=[self.id])
  
 
    
